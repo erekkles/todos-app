e85bec7f5bf5fa0ce77aaed4f25d0199932ffd09
Franco <72584397+erekkles@users.noreply.github.com>
2022-02-19 13:59:18 -0300

    Fix typo

--- client/src/providers/GlobalProvider.jsx
+++ client/src/providers/GlobalProvider.jsx
@@ -5,7 +5,7 @@ export const GlobalContext = React.createContext({});
 const GlobalProvider = ({ children }) => {
     const [filter, setFilter] = useState('all');
     const [search, setSearch] = useState('')
-    const svBaseUrl = 'https://todos-mernstack-app.herokuapp.com/';
+    const svBaseUrl = 'https://todos-mernstack-app.herokuapp.com';
 
     const saveUsername = (user) =>  window.localStorage.setItem('username', user);
 
